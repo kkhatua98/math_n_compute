d63dd9790975c8a8132fbd4ba80d24a38b49cfb0
Your Name <you@example.com>
2024-09-07 22:31:06 +0530

    Some version

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,19 +1,40 @@
 window.onload = function () {
-    document.getElementById("toc_sidebar").addEventListener("click", function(){ 
-        element = document.getElementById("toc")
-        const basis = element.style.flexBasis
-        console.log(basis)
-        if (basis == "25%")
-        {
-            element.style.flexBasis = "5%"
-            document.querySelectorAll("details").forEach(elem => elem.style.display = "none")
-        }
-        else
-        {
-            element.style.flexBasis = "25%";
-            document.querySelectorAll("details").forEach(elem => elem.style.display = "block")
-        }
-    })
+    // document.getElementById("toc_sidebar").addEventListener("click", function(){ 
+    //     element = document.getElementById("toc")
+    //     const basis = element.style.flexBasis
+    //     console.log(basis)
+    //     if (basis == "25%")
+    //     {
+    //         element.style.flexBasis = "5%"
+    //         document.querySelectorAll("details").forEach(elem => elem.style.display = "none")
+    //     }
+    //     else
+    //     {
+    //         element.style.flexBasis = "25%";
+    //         document.querySelectorAll("details").forEach(elem => elem.style.display = "block")
+    //     }
+    // })
+
+    var button = document.getElementById('mini_toc_button'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('toc');
+    console.log(div.style.display);
+    console.log(window.getComputedStyle(document.getElementById('toc')).display);
+    var display = window.getComputedStyle(document.getElementById('toc')).display;
+    if (display != 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.width = '25%';
+        div.style.display = 'flex';
+        var nodes = div.childNodes;
+        // for(var i=0; i<nodes.length; i++) {
+        //     console.log(nodes[i].style.display);
+        // }
+        // div.child.style.display = 'none';
+    }
+};
 }
 
 // .toc { flex-basis: 5%; }
